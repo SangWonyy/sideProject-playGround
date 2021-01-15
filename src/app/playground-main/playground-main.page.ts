bf0b3485feb9f1631b464eea6458d7060ff2311d
sangwonyu <havedreamm@naver.com>
2021-01-15 15:40:33 +0900

    scroll bug ing

--- src/app/playground-main/playground-main.page.ts
+++ src/app/playground-main/playground-main.page.ts
@@ -15,7 +15,7 @@ export class PlaygroundMainPage implements OnInit {
   public acc = 0.2;
   public delayedYOffset = 0;
   public rafId;
-  public rafState;
+  public rafState = true;
   public sceneInfo;
   @HostListener('window:load')
   playMain() {
@@ -131,14 +131,13 @@ export class PlaygroundMainPage implements OnInit {
         }
       }
     ];
-    this.setCanvasImages();
     this.setLayout(); // 중간에 새로고침 시, 콘텐츠 양에 따라 높이 계산에 오차가 발생하는 경우를 방지하기 위해 before-load 클래스 제거 전에도 확실하게 높이를 세팅하도록 한번 더 실행
-    // document.querySelector(`#show-scene-${this.currentScene}`).classList.remove('before-load');
-    // this.setLayout();
 
+    const img = new Image();
+    img.src = `assets/main/video/001/IMG_6726.JPG`;
     const context = this.sceneInfo[0].objs.canvas.getContext('2d');
-    this.sceneInfo[0].objs.videoImages[0].onload = (() => {
-      context.drawImage(this.sceneInfo[0].objs.videoImages[0], 0, 0);
+    img.onload = (() => {
+      context.drawImage(img, 0, 0);
     })
 
     // 중간에서 새로고침 했을 경우 자동 스크롤로 제대로 그려주기
@@ -187,31 +186,6 @@ export class PlaygroundMainPage implements OnInit {
   }
   async goMain() {
     await this.app.go('img-upload');
-  }
-
-  setCanvasImages() {
-    let imgElem;
-    for (let i = 0; i < this.sceneInfo[0].values.videoImageCount; i++) {
-      imgElem = new Image();
-      imgElem.src = `assets/main/video/001/IMG_${6726 + i}.JPG`;
-      this.sceneInfo[0].objs.videoImages.push(imgElem);
-    }
-
-    let imgElem2;
-    for (let i = 0; i < this.sceneInfo[2].values.videoImageCount; i++) {
-      imgElem2 = new Image();
-      imgElem2.src = `assets/main/video/002/IMG_${7027 + i}.JPG`;
-      this.sceneInfo[2].objs.videoImages.push(imgElem2);
-    }
-
-    let imgElem3;
-    for (let i = 0; i < this.sceneInfo[3].objs.imagesPath.length; i++) {
-      imgElem3 = new Image();
-      imgElem3.src = this.sceneInfo[3].objs.imagesPath[i];
-      this.sceneInfo[3].objs.images.push(imgElem3);
-    }
-
-    console.log(this.sceneInfo);
   }
 
   checkMenu() {
@@ -408,7 +382,12 @@ export class PlaygroundMainPage implements OnInit {
 
           canvas.style.transform = `scale(${canvasScaleRatio})`;
           context.fillStyle = 'white';
-          context.drawImage(objs.images[0], 0, 0);
+          const img = new Image();
+          img.src = 'assets/main/image/blend-image-1.jpg';
+          img.onload = (() => {
+            context.drawImage(img, 0, 0);
+          })
+
 
           // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
           const recalculatedInnerWidth = this.platform.width() / canvasScaleRatio;
@@ -455,7 +434,11 @@ export class PlaygroundMainPage implements OnInit {
 
         canvas.style.transform = `scale(${canvasScaleRatio})`;
         context.fillStyle = 'white';
-        context.drawImage(objs.images[0], 0, 0);
+        const img = new Image();
+        img.src = 'assets/main/image/blend-image-1.jpg';
+        img.onload = (() => {
+          context.drawImage(img, 0, 0);
+        })
 
         // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
         const recalculatedInnerWidth = this.platform.width() / canvasScaleRatio;
@@ -511,11 +494,13 @@ export class PlaygroundMainPage implements OnInit {
           // @ts-ignore
           values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
           const blendHeight = this.calcValues(values.blendHeight, currentYOffset);
-
-          context.drawImage(objs.images[1],
-              0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
-              0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
-          );
+          const img = new Image();
+          img.src = 'assets/main/image/blend-image-2.jpg';
+          img.onload = (() => {
+            context.drawImage(img, 0,
+                objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+                0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight);
+          })
 
           objs.canvas.classList.add('sticky');
           objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
@@ -595,21 +580,27 @@ export class PlaygroundMainPage implements OnInit {
   }
 
   softLoop() {
-    const canvas = this.sceneInfo[this.currentScene].objs.canvas as HTMLCanvasElement;
-    const context = canvas.getContext('2d');
+    const canvas = this.sceneInfo[this.currentScene].objs.canvas;
+    const context = this.sceneInfo[this.currentScene].objs.canvas.getContext('2d');
     this.delayedYOffset = this.delayedYOffset + (this.yOffset - this.delayedYOffset) * this.acc;
-
+    let img = new Image();
     if (!this.enterNewScene) {
       if (this.currentScene === 0 || this.currentScene === 2) {
         const currentYOffset = this.delayedYOffset - this.prevScrollHeight;
         const objs = this.sceneInfo[this.currentScene].objs;
         const values = this.sceneInfo[this.currentScene].values;
         let sequence = Math.round(this.calcValues(values.imageSequence, currentYOffset));
-        if (objs.videoImages[sequence]) {
-          objs.videoImages[sequence].onload = (() => {
-            context.drawImage(objs.videoImages[sequence], 0, 0);
-          })
+        console.log(sequence);
+        console.log(objs.videoImages[sequence])
+        if(this.currentScene === 0) {
+          img.src = `assets/main/video/001/IMG_${6726 + sequence}.JPG`;
+        } else if (this.currentScene === 2) {
+          img.src = `assets/main/video/002/IMG_${7027 + sequence}.JPG`;
         }
+        const context = this.sceneInfo[0].objs.canvas.getContext('2d');
+        img.onload = (() => {
+          context.drawImage(img, 0, 0);
+        })
       }
     }
 
@@ -618,44 +609,33 @@ export class PlaygroundMainPage implements OnInit {
     if (this.delayedYOffset < 1) {
       this.scrollLoop();
       canvas.style.opacity = "1";
-      this.sceneInfo[0].objs.videoImages[0].onload = (() => {
-        context.drawImage(this.sceneInfo[0].objs.videoImages[0], 0, 0);
+      img.src = `assets/main/video/001/IMG_6726.JPG`;
+      img.onload = (() => {
+        context.drawImage(img, 0, 0);
       });
     }
     // 페이지 맨 아래로 갈 경우: 마지막 섹션은 스크롤 계산으로 위치 및 크기를 결정해야할 요소들이 많아서 1픽셀을 움직여주는 것으로 해결
     if ((this.platform.height() - window.innerHeight) - this.delayedYOffset < 1) {
-      let tempYOffset = this.yOffset;
-      scrollTo(0, tempYOffset - 1);
+      scrollTo(0, 0);
     }
-console.log('softLoop', this.rafId);
-    // @ts-ignore
-    this.rafId = requestAnimationFrame(this.softLoop());
 
-    if (Math.abs(this.yOffset - this.delayedYOffset) < 1) {
-      console.log('cancel Loop')
-      cancelAnimationFrame(this.rafId);
-      this.rafState = false;
+    if (Math.abs(this.yOffset - this.delayedYOffset) > 1) {
+      this.requestAni();
     }
   }
 
-
-
-  scrollEvent() {
-    console.log('scroll')
-    this.yOffset = window.pageYOffset;
+  scrollEvent(scrollTop) {
+    this.yOffset = scrollTop;
     this.scrollLoop();
     this.checkMenu();
-
-    if (!this.rafState) {
-      // @ts-ignore
-      this.rafId = requestAnimationFrame(this.softLoop());
-      this.rafState = true;
-    }
+    this.softLoop();
   }
+
+  requestAni() {
+    this.softLoop();
+  }
+
   //
-
-
-
 
   // transitionendEvent() {
   //   document.querySelector('.loading').addEventListener('transitionend', (e) => {
