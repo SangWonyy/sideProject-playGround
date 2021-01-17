211db202f4d8d3bf7706e95c05b065e8e8ad1948
sangwonyu <havedreamm@naver.com>
2021-01-18 06:46:25 +0900

    delete other scene

--- src/app/playground-main/playground-main.page.ts
+++ src/app/playground-main/playground-main.page.ts
@@ -26,11 +26,11 @@ export class PlaygroundMainPage implements OnInit {
         heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
         scrollHeight: 0,
         objs: {
-          container: document.querySelector('#scroll-section-1') as HTMLElement,
-          messageA: document.querySelector('#scroll-section-1 .main-message.a') as HTMLElement,
-          messageB: document.querySelector('#scroll-section-1 .main-message.b') as HTMLElement,
-          messageC: document.querySelector('#scroll-section-1 .main-message.c') as HTMLElement,
-          messageD: document.querySelector('#scroll-section-1 .main-message.d') as HTMLElement,
+          container: document.querySelector('#scroll-section-0') as HTMLElement,
+          messageA: document.querySelector('#scroll-section-0 .main-message.a') as HTMLElement,
+          messageB: document.querySelector('#scroll-section-0 .main-message.b') as HTMLElement,
+          messageC: document.querySelector('#scroll-section-0 .main-message.c') as HTMLElement,
+          messageD: document.querySelector('#scroll-section-0 .main-message.d') as HTMLElement,
           canvas: document.querySelector('#video-canvas-0') as HTMLCanvasElement,
           // context: document.querySelector('#video-canvas-0').getContext('2d'),
           videoImages: []
@@ -55,79 +55,6 @@ export class PlaygroundMainPage implements OnInit {
           messageB_translateY_out: [0, -20, { start: 0.45, end: 0.5 }],
           messageC_translateY_out: [0, -20, { start: 0.65, end: 0.7 }],
           messageD_translateY_out: [0, -20, { start: 0.85, end: 0.9 }]
-        }
-      },
-      {
-        // 1
-        type: 'normal',
-        // heightNum: 5, // type normal에서는 필요 없음
-        scrollHeight: 0,
-        objs: {
-          container: document.querySelector('#scroll-section-1-5') as HTMLElement,
-          content: document.querySelector('#scroll-section-1-5 .description') as HTMLElement
-        }
-      },
-      {
-        // 2
-        type: 'sticky',
-        heightNum: 5,
-        scrollHeight: 0,
-        objs: {
-          container: document.querySelector('#scroll-section-2') as HTMLElement,
-          messageA: document.querySelector('#scroll-section-2 .a') as HTMLElement,
-          messageB: document.querySelector('#scroll-section-2 .b') as HTMLElement,
-          messageC: document.querySelector('#scroll-section-2 .c') as HTMLElement,
-          pinB: document.querySelector('#scroll-section-2 .b .pin') as HTMLElement,
-          pinC: document.querySelector('#scroll-section-2 .c .pin') as HTMLElement,
-          canvas: document.querySelector('#video-canvas-1') as HTMLCanvasElement,
-          // context: document.querySelector('#video-canvas-1').getContext('2d'),
-          videoImages: []
-        },
-        values: {
-          videoImageCount: 960,
-          imageSequence: [0, 959],
-          canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
-          canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
-          messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
-          messageB_translateY_in: [30, 0, { start: 0.6, end: 0.65 }],
-          messageC_translateY_in: [30, 0, { start: 0.87, end: 0.92 }],
-          messageA_opacity_in: [0, 1, { start: 0.25, end: 0.3 }],
-          messageB_opacity_in: [0, 1, { start: 0.6, end: 0.65 }],
-          messageC_opacity_in: [0, 1, { start: 0.87, end: 0.92 }],
-          messageA_translateY_out: [0, -20, { start: 0.4, end: 0.45 }],
-          messageB_translateY_out: [0, -20, { start: 0.68, end: 0.73 }],
-          messageC_translateY_out: [0, -20, { start: 0.95, end: 1 }],
-          messageA_opacity_out: [1, 0, { start: 0.4, end: 0.45 }],
-          messageB_opacity_out: [1, 0, { start: 0.68, end: 0.73 }],
-          messageC_opacity_out: [1, 0, { start: 0.95, end: 1 }],
-          pinB_scaleY: [0.5, 1, { start: 0.6, end: 0.65 }],
-          pinC_scaleY: [0.5, 1, { start: 0.87, end: 0.92 }]
-        }
-      },
-      {
-        // 3
-        type: 'sticky',
-        heightNum: 5,
-        scrollHeight: 0,
-        objs: {
-          container: document.querySelector('#scroll-section-3') as HTMLElement,
-          canvasCaption: document.querySelector('.canvas-caption') as HTMLElement,
-          canvas: document.querySelector('.image-blend-canvas') as HTMLCanvasElement,
-          // context: document.querySelector('.image-blend-canvas').getContext('2d'),
-          imagesPath: [
-            'assets/main/image/blend-image-1.jpg',
-            'assets/main/image/blend-image-2.jpg'
-          ],
-          images: []
-        },
-        values: {
-          rect1X: [ 0, 0, { start: 0, end: 0 } ],
-          rect2X: [ 0, 0, { start: 0, end: 0 } ],
-          blendHeight: [ 0, 0, { start: 0, end: 0 } ],
-          canvas_scale: [ 0, 0, { start: 0, end: 0 } ],
-          canvasCaption_opacity: [ 0, 1, { start: 0, end: 0 } ],
-          canvasCaption_translateY: [ 20, 0, { start: 0, end: 0 } ],
-          rectStartY: 0
         }
       }
     ];
@@ -212,18 +139,8 @@ export class PlaygroundMainPage implements OnInit {
 
     this.yOffset = window.pageYOffset;
 
-    let totalScrollHeight = 0;
-    for (let i = 0; i < this.sceneInfo.length; i++) {
-      totalScrollHeight += this.sceneInfo[i].scrollHeight;
-      if (totalScrollHeight >= this.yOffset) {
-        this.currentScene = i;
-        break;
-      }
-    }
-
     const heightRatio = window.innerHeight / 1080;
     this.sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
-    this.sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
   }
 
   calcValues(values, currentYOffset) {
@@ -255,8 +172,6 @@ export class PlaygroundMainPage implements OnInit {
   playAnimation() {
     const objs = this.sceneInfo[this.currentScene].objs;
     const values = this.sceneInfo[this.currentScene].values;
-    let canvas;
-    let context;
     const messageA = objs.messageA as HTMLElement;
     const messageB = objs.messageB as HTMLElement;
     const messageC = objs.messageC as HTMLElement;
@@ -267,10 +182,8 @@ export class PlaygroundMainPage implements OnInit {
     const currentYOffset = this.yOffset - this.prevScrollHeight;
     const scrollHeight = this.sceneInfo[this.currentScene].scrollHeight;
     const scrollRatio = currentYOffset / scrollHeight;
-    if(this.currentScene !== 1) {
-      canvas = objs.canvas as HTMLCanvasElement;
-      context = canvas.getContext('2d');
-    }
+    const canvas = objs.canvas as HTMLCanvasElement;
+    const context = canvas.getContext('2d');
     switch (this.currentScene) {
       case 0:
         canvas.style.opacity = this.calcValues(values.canvas_opacity, currentYOffset);
@@ -316,237 +229,13 @@ export class PlaygroundMainPage implements OnInit {
         }
 
         break;
-
-      case 2:
-        // let sequence2 = Math.round(calcValues(values.imageSequence, currentYOffset));
-        // objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
-
-        if (scrollRatio <= 0.5) {
-          // in
-          objs.canvas.style.opacity = this.calcValues(values.canvas_opacity_in, currentYOffset);
-        } else {
-          // out
-          objs.canvas.style.opacity = this.calcValues(values.canvas_opacity_out, currentYOffset);
-        }
-
-        if (scrollRatio <= 0.32) {
-          // in
-          messageA.style.opacity = this.calcValues(values.messageA_opacity_in, currentYOffset);
-          messageA.style.transform = `translate3d(0, ${this.calcValues(values.messageA_translateY_in, currentYOffset)}%, 0)`;
-        } else {
-          // out
-          messageA.style.opacity = this.calcValues(values.messageA_opacity_out, currentYOffset);
-          messageA.style.transform = `translate3d(0, ${this.calcValues(values.messageA_translateY_out, currentYOffset)}%, 0)`;
-        }
-
-        if (scrollRatio <= 0.67) {
-          // in
-          messageB.style.transform = `translate3d(0, ${this.calcValues(values.messageB_translateY_in, currentYOffset)}%, 0)`;
-          messageB.style.opacity = this.calcValues(values.messageB_opacity_in, currentYOffset);
-          pinB.style.transform = `scaleY(${this.calcValues(values.pinB_scaleY, currentYOffset)})`;
-        } else {
-          // out
-          messageB.style.transform = `translate3d(0, ${this.calcValues(values.messageB_translateY_out, currentYOffset)}%, 0)`;
-          messageB.style.opacity = this.calcValues(values.messageB_opacity_out, currentYOffset);
-          pinB.style.transform = `scaleY(${this.calcValues(values.pinB_scaleY, currentYOffset)})`;
-        }
-
-        if (scrollRatio <= 0.93) {
-          // in
-          messageC.style.transform = `translate3d(0, ${this.calcValues(values.messageC_translateY_in, currentYOffset)}%, 0)`;
-          messageC.style.opacity = this.calcValues(values.messageC_opacity_in, currentYOffset);
-          pinC.style.transform = `scaleY(${this.calcValues(values.pinC_scaleY, currentYOffset)})`;
-        } else {
-          // out
-          messageC.style.transform = `translate3d(0, ${this.calcValues(values.messageC_translateY_out, currentYOffset)}%, 0)`;
-          messageC.style.opacity = this.calcValues(values.messageC_opacity_out, currentYOffset);
-          pinC.style.transform = `scaleY(${this.calcValues(values.pinC_scaleY, currentYOffset)})`;
-        }
-
-        // this.currentScene 3에서 쓰는 캔버스를 미리 그려주기 시작
-        if (scrollRatio > 0.9) {
-          const objs = this.sceneInfo[3].objs;
-          const values = this.sceneInfo[3].values;
-          const widthRatio = window.innerWidth / objs.canvas.width;
-          const heightRatio = window.innerHeight / objs.canvas.height;
-          let canvasScaleRatio;
-
-          if (widthRatio <= heightRatio) {
-            // 캔버스보다 브라우저 창이 홀쭉한 경우
-            canvasScaleRatio = heightRatio;
-          } else {
-            // 캔버스보다 브라우저 창이 납작한 경우
-            canvasScaleRatio = widthRatio;
-          }
-
-          canvas.style.transform = `scale(${canvasScaleRatio})`;
-          context.fillStyle = 'white';
-          const img = new Image();
-          img.src = `assets/main/video/002/IMG_7027.JPG`;
-          img.onload = (() => {
-            context.drawImage(img, 0, 0);
-          })
-
-
-          // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
-          const recalculatedInnerWidth = this.platform.width() / canvasScaleRatio;
-          const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
-
-          const whiteRectWidth = recalculatedInnerWidth * 0.15;
-          values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
-          values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
-          values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
-          values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
-
-          // 좌우 흰색 박스 그리기
-          context.fillRect(
-              Math.round(values.rect1X[0]),
-              0,
-              Math.round(whiteRectWidth),
-              objs.canvas.height
-          );
-          context.fillRect(
-              Math.round(values.rect2X[0]),
-              0,
-              Math.round(whiteRectWidth),
-              objs.canvas.height
-          );
-        }
-
-        break;
-
-      case 3:
-
-        let step = 0;
-        // 가로/세로 모두 꽉 차게 하기 위해 여기서 세팅(계산 필요)
-        const widthRatio = window.innerWidth / objs.canvas.width;
-        const heightRatio = window.innerHeight / objs.canvas.height;
-        let canvasScaleRatio;
-
-        if (widthRatio <= heightRatio) {
-          // 캔버스보다 브라우저 창이 홀쭉한 경우
-          canvasScaleRatio = heightRatio;
-        } else {
-          // 캔버스보다 브라우저 창이 납작한 경우
-          canvasScaleRatio = widthRatio;
-        }
-
-        canvas.style.transform = `scale(${canvasScaleRatio})`;
-        context.fillStyle = 'white';
-        const img = new Image();
-        img.src = 'assets/main/image/blend-image-1.jpg';
-        img.onload = (() => {
-          context.drawImage(img, 0, 0);
-        })
-
-        // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
-        const recalculatedInnerWidth = this.platform.width() / canvasScaleRatio;
-        const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
-
-        if (!values.rectStartY) {
-          // values.rectStartY = objs.canvas.getBoundingClientRect().top;
-          values.rectStartY = objs.canvas.offsetTop + (objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2;
-          // @ts-ignore
-          values.rect1X[2].start = (window.innerHeight / 2) / scrollHeight;
-          // @ts-ignore
-          values.rect2X[2].start = (window.innerHeight / 2) / scrollHeight;
-          // @ts-ignore
-          values.rect1X[2].end = values.rectStartY / scrollHeight;
-          // @ts-ignore
-          values.rect2X[2].end = values.rectStartY / scrollHeight;
-        }
-
-        const whiteRectWidth = recalculatedInnerWidth * 0.15;
-        values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
-        values.rect1X[1] = values.rect1X[0] - whiteRectWidth;
-        values.rect2X[0] = values.rect1X[0] + recalculatedInnerWidth - whiteRectWidth;
-        values.rect2X[1] = values.rect2X[0] + whiteRectWidth;
-
-        // 좌우 흰색 박스 그리기
-        context.fillRect(
-            parseInt(this.calcValues(values.rect1X, currentYOffset)),
-            0,
-            Math.round(whiteRectWidth),
-            objs.canvas.height
-        );
-        context.fillRect(
-            parseInt(this.calcValues(values.rect2X, currentYOffset)),
-            0,
-            Math.round(whiteRectWidth),
-            objs.canvas.height
-        );
-
-        // @ts-ignore
-        if (scrollRatio < values.rect1X[2].end) {
-          step = 1;
-          objs.canvas.classList.remove('sticky');
-        } else {
-          step = 2;
-          // 이미지 블렌드
-          // values.blendHeight: [ 0, 0, { start: 0, end: 0 } ]
-          values.blendHeight[0] = 0;
-          values.blendHeight[1] = objs.canvas.height;
-          // @ts-ignore
-          values.blendHeight[2].start = values.rect1X[2].end;
-          // @ts-ignore
-          values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
-          const blendHeight = this.calcValues(values.blendHeight, currentYOffset);
-          const img = new Image();
-          img.src = 'assets/main/image/blend-image-2.jpg';
-          img.onload = (() => {
-            context.drawImage(img, 0,
-                objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
-                0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight);
-          })
-
-          objs.canvas.classList.add('sticky');
-          objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
-          // @ts-ignore
-          if (scrollRatio > values.blendHeight[2].end) {
-            values.canvas_scale[0] = canvasScaleRatio;
-            values.canvas_scale[1] = this.platform.width() / (1.5 * objs.canvas.width);
-            // @ts-ignore
-            values.canvas_scale[2].start = values.blendHeight[2].end;
-            // @ts-ignore
-            values.canvas_scale[2].end = values.canvas_scale[2].start + 0.2;
-
-            canvas.style.transform = `scale(${this.calcValues(values.canvas_scale, currentYOffset)})`;
-            canvas.style.marginTop = "0";
-          }
-
-          // @ts-ignore
-          if (scrollRatio > values.canvas_scale[2].end
-              // @ts-ignore
-              && values.canvas_scale[2].end > 0) {
-            objs.canvas.classList.remove('sticky');
-            objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
-            // @ts-ignore
-            values.canvasCaption_opacity[2].start = values.canvas_scale[2].end;
-            // @ts-ignore
-            values.canvasCaption_opacity[2].end = values.canvasCaption_opacity[2].start + 0.1;
-            // @ts-ignore
-            values.canvasCaption_translateY[2].start = values.canvasCaption_opacity[2].star
-            // @ts-ignorevalues.canvasCaption_translateY[2].end = values.canvasCaption_opacity[2].end;
-            canvasCaption.style.opacity = this.calcValues(values.canvasCaption_opacity, currentYOffset);
-            canvasCaption.style.transform = `translate3d(0, ${this.calcValues(values.canvasCaption_translateY, currentYOffset)}%, 0)`;
-          } else {
-            // @ts-ignore
-            canvasCaption.style.opacity = values.canvasCaption_opacity[0];
-          }
-        }
-
-        break;
     }
   }
 
   scrollLoop() {
     this.enterNewScene = false;
     this.prevScrollHeight = 0;
-    let currentContent = document.querySelector(`#show-scene-${this.currentScene}`);
-    if (!currentContent) {
-      document.querySelector(`.mainContent`).setAttribute('id', `show-scene-${this.currentScene}`)
-      currentContent = document.querySelector(`#show-scene-${this.currentScene}`);
-    }
+    let currentContent = document.querySelector(`#show-scene-0`);
     for (let i = 0; i < this.currentScene; i++) {
       this.prevScrollHeight += this.sceneInfo[i].scrollHeight;
     }
@@ -586,20 +275,18 @@ export class PlaygroundMainPage implements OnInit {
     this.delayedYOffset = this.delayedYOffset + (this.yOffset - this.delayedYOffset) * this.acc;
     let img = new Image();
     if (!this.enterNewScene) {
-      if (this.currentScene === 0 || this.currentScene === 2) {
+      if (this.currentScene === 0) {
         const currentYOffset = this.delayedYOffset - this.prevScrollHeight;
         const objs = this.sceneInfo[this.currentScene].objs;
         const values = this.sceneInfo[this.currentScene].values;
         let sequence = Math.round(this.calcValues(values.imageSequence, currentYOffset));
         if(this.currentScene === 0 && (1 + sequence) <= 300 && (1 + sequence) >= 1) {
           img.src = `assets/main/video/001/IMG_ (${1 + sequence}).jpg`;
-        } else if (this.currentScene === 2 && (7027 + sequence) <= 7986 && (7027 + sequence) >= 7027) {
-          img.src = `assets/main/video/002/IMG_${7027 + sequence}.JPG`;
+          const context = this.sceneInfo[this.currentScene].objs.canvas.getContext('2d');
+          img.onload = (() => {
+            context.drawImage(img, 0, 0);
+          })
         }
-        const context = this.sceneInfo[this.currentScene].objs.canvas.getContext('2d');
-        img.onload = (() => {
-          context.drawImage(img, 0, 0);
-        })
       }
     }
 
@@ -619,7 +306,7 @@ export class PlaygroundMainPage implements OnInit {
     }
 
     if (Math.abs(this.yOffset - this.delayedYOffset) > 1) {
-      if(this.currentScene === 0 || this.currentScene === 2) {
+      if(this.currentScene === 0) {
         this.requestAni();
       }
     }
@@ -629,21 +316,7 @@ export class PlaygroundMainPage implements OnInit {
     this.yOffset = scrollTop;
     this.scrollLoop();
     this.checkMenu();
-    if(this.currentScene !== 1) {
-      this.softLoop();
-    } else {
-      let totalScrollHeight = 0;
-      for (let i = 0; i < this.sceneInfo.length; i++) {
-        totalScrollHeight += this.sceneInfo[i].scrollHeight;
-        if (totalScrollHeight >= this.yOffset) {
-          if(i === 2) {
-            this.delayedYOffset = this.delayedYOffset + (this.yOffset - this.delayedYOffset) * this.acc;
-          }
-          this.currentScene = i;
-          break;
-        }
-      }
-    }
+    this.softLoop()
   }
 
   requestAni() {
