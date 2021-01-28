fdd73a435c80e861d4d7c7bd09df5039276b3b5e
sangwonyu <havedreamm@naver.com>
2021-01-29 06:12:32 +0900

    delete animation

--- src/app/playground-main/playground-main.page.ts
+++ src/app/playground-main/playground-main.page.ts
@@ -106,32 +106,10 @@ export class PlaygroundMainPage implements OnInit {
   }
 
   ngOnInit() {
-    this.resize$.pipe(
-        throttleTime( 500)
-    ).subscribe(
-        (result) => {
-          this.drawCanvasImg(this);
-        }
-    )
   }
 
   ionViewDidEnter() {
     this.checkMenu();
-  }
-
-  drawCanvasImg(self) {
-    const img = new Image();
-    const context = self.sceneInfo[0].objs.canvas.getContext('2d');
-    this.sceneInfo[self.currentScene].objs.canvas.width = self.platform.width();
-    const currentYOffset = self.delayedYOffset - self.prevScrollHeight;
-    const values = self.sceneInfo[self.currentScene].values;
-    let sequence = Math.round(self.calcValues(values.imageSequence, currentYOffset));
-    if(this.currentScene === 0 && (1 + sequence) <= 300 && (1 + sequence) >= 1) {
-      img.src = `assets/main/video/001/IMG_ (${1 + sequence}).jpg`;
-      img.onload = (() => {
-        context.drawImage(img, 0, 0, self.platform.width(), self.platform.height());
-      })
-    }
   }
 
   async goMain() {
@@ -193,166 +171,9 @@ export class PlaygroundMainPage implements OnInit {
     return rv;
   }
 
-  playAnimation() {
-    const objs = this.sceneInfo[this.currentScene].objs;
-    const values = this.sceneInfo[this.currentScene].values;
-    const messageA = objs.messageA as HTMLElement;
-    const messageB = objs.messageB as HTMLElement;
-    const messageC = objs.messageC as HTMLElement;
-    const messageD = objs.messageD as HTMLElement;
-    const pinB = objs.pinB as HTMLElement;
-    const pinC = objs.pinC as HTMLElement;
-    const canvasCaption = objs.canvasCaption as HTMLElement;
-    const currentYOffset = this.yOffset - this.prevScrollHeight;
-    const scrollHeight = this.sceneInfo[this.currentScene].scrollHeight;
-    const scrollRatio = currentYOffset / scrollHeight;
-    const canvas = objs.canvas as HTMLCanvasElement;
-    const context = canvas.getContext('2d');
-    switch (this.currentScene) {
-      case 0:
-        canvas.style.opacity = this.calcValues(values.canvas_opacity, currentYOffset);
-
-        if (scrollRatio <= 0.22) {
-          // in
-          messageA.style.opacity = this.calcValues(values.messageA_opacity_in, currentYOffset);
-          messageA.style.transform = `translate3d(0, ${this.calcValues(values.messageA_translateY_in, currentYOffset)}%, 0)`;
-        } else {
-          // out
-          messageA.style.opacity = this.calcValues(values.messageA_opacity_out, currentYOffset);
-          messageA.style.transform = `translate3d(0, ${this.calcValues(values.messageA_translateY_out, currentYOffset)}%, 0)`;
-        }
-
-        if (scrollRatio <= 0.42) {
-          // in
-          messageB.style.opacity = this.calcValues(values.messageB_opacity_in, currentYOffset);
-          messageB.style.transform = `translate3d(0, ${this.calcValues(values.messageB_translateY_in, currentYOffset)}%, 0)`;
-        } else {
-          // out
-          messageB.style.opacity = this.calcValues(values.messageB_opacity_out, currentYOffset);
-          messageB.style.transform = `translate3d(0, ${this.calcValues(values.messageB_translateY_out, currentYOffset)}%, 0)`;
-        }
-
-        if (scrollRatio <= 0.62) {
-          // in
-          messageC.style.opacity = this.calcValues(values.messageC_opacity_in, currentYOffset);
-          messageC.style.transform = `translate3d(0, ${this.calcValues(values.messageC_translateY_in, currentYOffset)}%, 0)`;
-        } else {
-          // out
-          messageC.style.opacity = this.calcValues(values.messageC_opacity_out, currentYOffset);
-          messageC.style.transform = `translate3d(0, ${this.calcValues(values.messageC_translateY_out, currentYOffset)}%, 0)`;
-        }
-
-        if (scrollRatio <= 0.82) {
-          // in
-          messageD.style.opacity = this.calcValues(values.messageD_opacity_in, currentYOffset);
-          messageD.style.transform = `translate3d(0, ${this.calcValues(values.messageD_translateY_in, currentYOffset)}%, 0)`;
-        } else {
-          // out
-          messageD.style.opacity = this.calcValues(values.messageD_opacity_out, currentYOffset);
-          messageD.style.transform = `translate3d(0, ${this.calcValues(values.messageD_translateY_out, currentYOffset)}%, 0)`;
-        }
-
-        break;
-    }
-  }
-
-  scrollLoop() {
-    this.enterNewScene = false;
-    this.prevScrollHeight = 0;
-    let currentContent = document.querySelector(`#show-scene-0`);
-    for (let i = 0; i < this.currentScene; i++) {
-      this.prevScrollHeight += this.sceneInfo[i].scrollHeight;
-    }
-
-    if (this.delayedYOffset < this.prevScrollHeight + this.sceneInfo[this.currentScene].scrollHeight) {
-      currentContent.classList.remove('scroll-effect-end');
-    }
-    if (this.delayedYOffset > this.prevScrollHeight + this.sceneInfo[this.currentScene].scrollHeight) {
-      this.enterNewScene = true;
-      if (this.currentScene === this.sceneInfo.length - 1) {
-        currentContent.classList.add('scroll-effect-end');
-      }
-
-      if (this.currentScene < this.sceneInfo.length - 1) {
-        this.currentScene++;
-      }
-      currentContent.setAttribute('id', `show-scene-${this.currentScene}`);
-    }
-
-    if (this.delayedYOffset < this.prevScrollHeight) {
-      this.enterNewScene = true;
-      // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
-      if (this.currentScene === 0) return;
-      // this.currentScene--;
-      currentContent.setAttribute('id', `show-scene-${this.currentScene}`);
-    }
-
-    if (this.enterNewScene) return;
-
-
-    this.playAnimation();
-  }
-
-  softLoop() {
-    const canvas = this.sceneInfo[this.currentScene].objs.canvas;
-    const context = this.sceneInfo[this.currentScene].objs.canvas.getContext('2d');
-    this.delayedYOffset = this.delayedYOffset + (this.yOffset - this.delayedYOffset) * this.acc;
-    let img = new Image();
-    if (!this.enterNewScene) {
-      if (this.currentScene === 0) {
-        const currentYOffset = this.delayedYOffset - this.prevScrollHeight;
-        const objs = this.sceneInfo[this.currentScene].objs;
-        const values = this.sceneInfo[this.currentScene].values;
-        let sequence = Math.round(this.calcValues(values.imageSequence, currentYOffset));
-        if(this.currentScene === 0 && (1 + sequence) <= 300 && (1 + sequence) >= 1) {
-          img.src = `assets/main/video/001/IMG_ (${1 + sequence}).jpg`;
-          img.onload = (() => {
-            context.drawImage(img, 0, 0, this.platform.width(), this.platform.height());
-          })
-        }
-      }
-    }
-
-    // 일부 기기에서 페이지 끝으로 고속 이동하면 body id가 제대로 인식 안되는 경우를 해결
-    // 페이지 맨 위로 갈 경우: scrollLoop와 첫 scene의 기본 캔버스 그리기 수행
-    if (this.delayedYOffset < 1) {
-      this.scrollLoop();
-      canvas.style.opacity = "1";
-      img.src = `assets/main/video/001/IMG_ (1).jpg`;
-      img.onload = (() => {
-        context.drawImage(img, 0, 0, this.platform.width(), this.platform.height());
-      })
-
-    }
-    // 페이지 맨 아래로 갈 경우: 마지막 섹션은 스크롤 계산으로 위치 및 크기를 결정해야할 요소들이 많아서 1픽셀을 움직여주는 것으로 해결
-    if ((this.platform.height() - window.innerHeight) - this.delayedYOffset < 1) {
-      scrollTo(0, 0);
-    }
-
-    if (Math.abs(this.yOffset - this.delayedYOffset) > 1) {
-      if(this.currentScene === 0) {
-        this.requestAni();
-      }
-    }
-  }
-
   scrollEvent(scrollTop) {
     this.yOffset = scrollTop;
-    this.scrollLoop();
     this.checkMenu();
-    this.softLoop()
   }
-
-  requestAni() {
-    this.softLoop();
-  }
-
-  //
-
-  // transitionendEvent() {
-  //   document.querySelector('.loading').addEventListener('transitionend', (e) => {
-  //     document.body.removeChild(<Node>e.currentTarget);
-  //   });
-  // }
 
 }
