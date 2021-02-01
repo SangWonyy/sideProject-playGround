394e3a7b0a41350e554fe7415660723499233ebe
sangwonyu <tkddnjs231@naver.com>
2021-02-02 06:36:37 +0900

    opacity in out

--- src/app/playground-main/playground-main.page.ts
+++ src/app/playground-main/playground-main.page.ts
@@ -32,31 +32,22 @@ export class PlaygroundMainPage implements OnInit {
         scrollHeight: 0,
         objs: {
           container: document.querySelector('#scroll-section-0') as HTMLElement,
-          messageA: document.querySelector('#scroll-section-0 .main-message.a') as HTMLElement,
-          messageB: document.querySelector('#scroll-section-0 .main-message.b') as HTMLElement,
-          messageC: document.querySelector('#scroll-section-0 .main-message.c') as HTMLElement,
-          messageD: document.querySelector('#scroll-section-0 .main-message.d') as HTMLElement,
+          messageA: document.querySelector('.sticky-elem.first') as HTMLElement,
+          messageB: document.querySelector('.sticky-elem.second') as HTMLElement,
+          messageC: document.querySelector('.sticky-elem.third') as HTMLElement
         },
         values: {
           videoImageCount: 300,
           imageSequence: [0, 299],
           canvas_opacity: [1, 0, { start: 0.9, end: 1 }],
-          messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+          messageA_opacity_in: [0, 1, { start: 0, end: 0.3 }],
           messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
           messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
           messageD_opacity_in: [0, 1, { start: 0.7, end: 0.8 }],
-          messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
-          messageB_translateY_in: [20, 0, { start: 0.3, end: 0.4 }],
-          messageC_translateY_in: [20, 0, { start: 0.5, end: 0.6 }],
-          messageD_translateY_in: [20, 0, { start: 0.7, end: 0.8 }],
-          messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+          messageA_opacity_out: [1, 0, { start: 0.35, end: 0.5 }],
           messageB_opacity_out: [1, 0, { start: 0.45, end: 0.5 }],
           messageC_opacity_out: [1, 0, { start: 0.65, end: 0.7 }],
           messageD_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
-          messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
-          messageB_translateY_out: [0, -20, { start: 0.45, end: 0.5 }],
-          messageC_translateY_out: [0, -20, { start: 0.65, end: 0.7 }],
-          messageD_translateY_out: [0, -20, { start: 0.85, end: 0.9 }]
         }
       }
     ];
@@ -65,7 +56,6 @@ export class PlaygroundMainPage implements OnInit {
     this.h1Position = (this.platform.height() + 100) / 2;
     h1Ele.setAttribute('style', `top: -${this.h1Position}px`);
     this.h1Position += h1Ele.clientHeight;
-
   }
 
   @HostListener('window:resize')
@@ -117,53 +107,80 @@ export class PlaygroundMainPage implements OnInit {
   }
 
   opacityMessage() {
-    const stickyElem = document.querySelector('.sticky-elem') as HTMLElement;
     if (this.yOffset > this.h1Position) {
-      
-      if (!stickyElem.style.top) {
+      const objs = this.sceneInfo[0].objs;
+      const values = this.sceneInfo[0].values;
+      const scrollHeight = this.sceneInfo[0].scrollHeight;
+      const scrollRatio = this.yOffset / scrollHeight;
+
+      if (!objs.messageA.style.top) {
         const stickyElePosition = ((this.platform.height() - 300) / 2);
-        stickyElem.style.top = `${stickyElePosition}px`;
+        objs.messageA.style.top = `${stickyElePosition}px`;
       }
-      if (+stickyElem.style.opacity <= 1) {
-        stickyElem.style.opacity = `${(this.yOffset - this.h1Position)/ 500 }`;
+
+      if (scrollRatio <= 0.46) {
+        // in
+        objs.messageA.style.opacity = this.calcValues(values.messageA_opacity_in);
+        
+      } else {
+          // out
+          objs.messageA.style.opacity = this.calcValues(values.messageA_opacity_out);
       }
-      
+  
+      // if (scrollRatio <= 0.55) {
+      //     // in
+      //     objs.messageB.style.opacity = this.calcValues(values.messageB_opacity_in, this.yOffset);
+      // } else {
+      //     // out
+      //     objs.messageB.style.opacity = this.calcValues(values.messageB_opacity_out, this.yOffset);
+      // }
+  
+      // if (scrollRatio <= 0.82) {
+      //     // in
+      //     objs.messageC.style.opacity = this.calcValues(values.messageC_opacity_in, this.yOffset);
+      // } else {
+      //     // out
+      //     objs.messageC.style.opacity = this.calcValues(values.messageC_opacity_out, this.yOffset);
+      // }
+  
+      // if (scrollRatio <= 0.82) {
+      //     // in
+      //     objs.messageD.style.opacity = this.calcValues(values.messageD_opacity_in, this.yOffset);
+      // } else {
+      //     // out
+      //     objs.messageD.style.opacity = this.calcValues(values.messageD_opacity_out, this.yOffset);
+      // }
     }
 
-    // if (this.h1Position + 800 > this.yOffset) {
-    //   stickyElem.style.opacity = `${(this.h1Position + 800 - this.yOffset)/ 500 }`;
-    // }
+    
   }
 
   scrollEvent(scrollTop) {
     this.yOffset = scrollTop;
-    console.log(this.yOffset);
-    console.log('platform height ::', this.platform.height())
     this.checkMenu();
     this.opacityMessage();
   }
 
-  calcValues(values, currentYOffset) {
+  calcValues(values) {
     let rv;
     // 현재 씬(스크롤섹션)에서 스크롤된 범위를 비율로 구하기
-    const scrollHeight = this.sceneInfo[0].scrollheight;
-    const scrollRatio = currentYOffset / scrollHeight;
+    const scrollHeight = this.sceneInfo[0].scrollHeight;
+    const scrollRatio = this.yOffset / scrollHeight;
 
     if (values.length === 3) {
         // start ~ end 사이에 애니메이션 실행
-        const partScrollStart = values[2].start * scrollHeight;
-        const partScrollEnd = values[2].end * scrollHeight;
+        const partScrollStart = ((scrollHeight - this.h1Position) * values[2].start) + this.h1Position;
+        const partScrollEnd = ((scrollHeight - this.h1Position) * values[2].end) + this.h1Position;
         const partScrollHeight = partScrollEnd - partScrollStart;
 
-        if (currentYOffset >= partScrollStart && currentYOffset <= partScrollEnd) {
-            rv = (currentYOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
-        } else if (currentYOffset < partScrollStart) {
+        console.log(`start : ${partScrollStart} , end: ${partScrollEnd} , yOffset : ${this.yOffset} , h1Position: ${this.h1Position}`);
+        if (this.yOffset >= partScrollStart && this.yOffset <= partScrollEnd) {
+            rv = (this.yOffset - partScrollStart) / partScrollHeight * (values[1] - values[0]) + values[0];
+        } else if (this.yOffset < partScrollStart) {
             rv = values[0];
-        } else if (currentYOffset > partScrollEnd) {
+        } else if (this.yOffset > partScrollEnd) {
             rv = values[1];
         }
-    } else {
-        rv = scrollRatio * (values[1] - values[0]) + values[0];
     }
 
     return rv;
