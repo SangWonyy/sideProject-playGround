849f52b903e88339c9cfee994a7e6031352233d7
sangwonyu <tkddnjs231@naver.com>
2021-02-10 06:37:02 +0900

    example matter.js

--- src/app/simple-game/simple-game.page.ts
+++ src/app/simple-game/simple-game.page.ts
@@ -19,13 +19,35 @@ export class SimpleGamePage implements OnInit {
     this.drawBlock();
   }
   drawBlock() {
+    let gameBox = document.querySelector('.gameBox');
+    let gameBoxWidth = gameBox.clientWidth - 1;
+    let gameBoxHeight = gameBox.clientHeight
     let engine = Matter.Engine.create();
     let render = Matter.Render.create({
-      element: document.querySelector('.gameBox'),
-      engine: engine
+      element: gameBox,
+      engine: engine,
+      options: {
+        width: gameBoxWidth,
+        height: gameBoxHeight
+      }
     });
 
-    let ground = Matter.Bodies.rectangle(400,600,810,60, {isStatic: true});
+    let ground = Matter.Bodies.rectangle(gameBoxWidth / 2, gameBoxHeight - 30, gameBoxWidth, 1, {isStatic: true});
+    let boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
+    let boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
+
+    let mouse = Matter.Mouse.create(render.canvas);
+    let mouseConstraint = Matter.MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        render: {visible: false}
+      }
+    });
+    render.mouse = mouse;
+
+    Matter.World.add(engine.world, [boxA, boxB, ground, mouseConstraint]);
+    Matter.Engine.run(engine);
+    Matter.Render.run(render);
   }
 
 }
