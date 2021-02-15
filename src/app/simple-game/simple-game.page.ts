f801212d3dc28e7f23d92c46f17912529ab7befb
sangwonyu <havedreamm@naver.com>
2021-02-15 09:35:47 +0900

    add ball

--- src/app/simple-game/simple-game.page.ts
+++ src/app/simple-game/simple-game.page.ts
@@ -19,35 +19,51 @@ export class SimpleGamePage implements OnInit {
     this.drawBlock();
   }
   drawBlock() {
-    let gameBox = document.querySelector('.gameBox');
-    let gameBoxWidth = gameBox.clientWidth - 1;
-    let gameBoxHeight = gameBox.clientHeight
-    let engine = Matter.Engine.create();
-    let render = Matter.Render.create({
-      element: gameBox,
-      engine: engine,
-      options: {
-        width: gameBoxWidth,
-        height: gameBoxHeight
-      }
-    });
+    try {
+      const gameBox = document.querySelector('.gameBox');
+      const gameBoxWidth = gameBox.clientWidth - 1;
+      const gameBoxHeight = gameBox.clientHeight;
+      const engine = Matter.Engine.create();
+      const render = Matter.Render.create({
+        element: gameBox,
+        engine,
+        options: {
+          width: gameBoxWidth,
+          height: gameBoxHeight,
+          wireframes: false
+        }
 
-    let ground = Matter.Bodies.rectangle(gameBoxWidth / 2, gameBoxHeight - 30, gameBoxWidth, 1, {isStatic: true});
-    let boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
-    let boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
+      });
 
-    let mouse = Matter.Mouse.create(render.canvas);
-    let mouseConstraint = Matter.MouseConstraint.create(engine, {
-      mouse: mouse,
-      constraint: {
-        render: {visible: false}
-      }
-    });
-    render.mouse = mouse;
+      const ground = Matter.Bodies.rectangle(gameBoxWidth / 2, gameBoxHeight - 30, gameBoxWidth, 1, {isStatic: true});
+      const boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
+      const boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
+      const ball = Matter.Bodies.circle(70, 100, 25, {
+        render: {
+          sprite: {
+            texture: '../../assets/game/ball.png',
+            xScale: 0.2,
+            yScale: 0.2
+          },
+        }
+      });
+      const mouse = Matter.Mouse.create(render.canvas);
+      const mouseConstraint = Matter.MouseConstraint.create(engine, {
+        mouse,
+        constraint: {
+          render: {visible: false}
+        }
+      });
+      render.mouse = mouse;
 
-    Matter.World.add(engine.world, [boxA, boxB, ground, mouseConstraint]);
-    Matter.Engine.run(engine);
-    Matter.Render.run(render);
+      Matter.World.add(engine.world, [boxA, boxB, ground, mouseConstraint, ball]);
+      Matter.Engine.run(engine);
+      Matter.Render.run(render);
+    } catch (e) {
+      console.error(e);
+    }
   }
+
+
 
 }
