94d021c7d177d132e59274d0662d2902f93da0cd
sangwonyu <havedreamm@naver.com>
2021-02-15 16:13:59 +0900

    draw wall obstacle

--- src/app/simple-game/simple-game.page.ts
+++ src/app/simple-game/simple-game.page.ts
@@ -16,47 +16,49 @@ export class SimpleGamePage implements OnInit {
 
 
   ionViewWillEnter() {
-    this.drawBlock();
+    this.playDefence();
   }
-  drawBlock() {
+  playDefence() {
     try {
-      const gameBox = document.querySelector('.gameBox');
-      const gameBoxWidth = gameBox.clientWidth - 1;
-      const gameBoxHeight = gameBox.clientHeight;
-      const engine = Matter.Engine.create();
-      const render = Matter.Render.create({
-        element: gameBox,
-        engine,
-        options: {
-          width: gameBoxWidth,
-          height: gameBoxHeight,
-          wireframes: false
-        }
+      const {engine, render, gameBoxWidth, gameBoxHeight} = this.createEngine();
 
+      const bottomWall = Matter.Bodies.rectangle(gameBoxWidth / 2, gameBoxHeight - 10, gameBoxWidth, 1, {isStatic: true});
+      const topWall = Matter.Bodies.rectangle(gameBoxWidth / 2, 10, gameBoxWidth, 1, {isStatic: true});
+      const leftWall = Matter.Bodies.rectangle(10, gameBoxHeight / 2, 1, gameBoxHeight, {isStatic: true});
+      const rightWall = Matter.Bodies.rectangle(gameBoxWidth - 10, gameBoxHeight / 2, 1, gameBoxHeight, {isStatic: true});
+      const obstacle1 = Matter.Bodies.rectangle(gameBoxWidth / 4, (gameBoxHeight + 10) / 3, 15, 25,  {
+        isStatic: true,
+        render: {
+          fillStyle: '#FF9F59',
+        }
+      });
+      const obstacle2 = Matter.Bodies.rectangle((gameBoxWidth * 2 + 30) / 3, (gameBoxHeight * 2) / 3, 15, 25,  {
+        isStatic: true,
+        render: {
+          fillStyle: '#FF9F59',
+        }
       });
 
-      const ground = Matter.Bodies.rectangle(gameBoxWidth / 2, gameBoxHeight - 30, gameBoxWidth, 1, {isStatic: true});
-      const boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
-      const boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
-      const ball = Matter.Bodies.circle(70, 100, 25, {
+      const obstacle3 = Matter.Bodies.circle(gameBoxWidth / 2, (gameBoxHeight) / 2, 15,  {
+        isStatic: true,
+        render: {
+          fillStyle: '#FF9F59',
+        }
+      });
+
+      const ball = Matter.Bodies.circle((gameBoxWidth / 2) - 12.5, 100, 25, {
         render: {
           sprite: {
             texture: '../../assets/game/ball.png',
             xScale: 0.2,
             yScale: 0.2
           },
-        }
+          // isStatic: true
+        },
+        frictionStatic: 1
       });
-      const mouse = Matter.Mouse.create(render.canvas);
-      const mouseConstraint = Matter.MouseConstraint.create(engine, {
-        mouse,
-        constraint: {
-          render: {visible: false}
-        }
-      });
-      render.mouse = mouse;
 
-      Matter.World.add(engine.world, [boxA, boxB, ground, mouseConstraint, ball]);
+      Matter.World.add(engine.world, [bottomWall, topWall, leftWall, rightWall, obstacle1, obstacle2, obstacle3, ball]);
       Matter.Engine.run(engine);
       Matter.Render.run(render);
     } catch (e) {
@@ -64,6 +66,24 @@ export class SimpleGamePage implements OnInit {
     }
   }
 
+  createEngine() {
+    const gameBox = document.querySelector('.gameBox');
+    const gameBoxWidth = gameBox.clientWidth - 1;
+    const gameBoxHeight = gameBox.clientHeight;
+    const engine = Matter.Engine.create();
+    const render = Matter.Render.create({
+      element: gameBox,
+      engine,
+      options: {
+        width: gameBoxWidth,
+        height: gameBoxHeight,
+        wireframes: false
+      }
+
+    });
+
+    return {engine, render, gameBoxWidth, gameBoxHeight};
+  }
 
 
 }
